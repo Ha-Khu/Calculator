70c661fffc70b14bf9be9da76b70d103047457c2
Dávid <141168015+Ha-Khu@users.noreply.github.com>
2023-08-30 13:32:17 +0200

    Added function for (add,subtract,multiply,divide)

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+function add (addNumber1, addNumber2) {
+    let sum = addNumber1 + addNumber2
+    return sum
+}
+
+function subtract (subtractNumber1, subtractNumber2) {
+    let sum = subtractNumber1 - subtractNumber2
+    return sum
+}
+
+function multiply (multiNumber1, multiNumber2) {
+    let sum = multiNumber1 * multiNumber2
+    return sum
+}
+
+function divide (divNumber1, divNumber2) {
+    let sum = divNumber1 / divNumber2
+    return sum
+}
+
+
+let firstNumber = 3
+let secondNumber = 5
+
