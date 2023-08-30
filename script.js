4c9528c933e7e3ce2b3c6ca812aaac56e4029cdf
Dávid <141168015+Ha-Khu@users.noreply.github.com>
2023-08-30 13:54:59 +0200

    Stored every btn in variables

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let number1_DOM = document.querySelector(".number-1")
+let number2_DOM = document.querySelector(".number-2")
+let number3_DOM = document.querySelector(".number-3")
+let number4_DOM = document.querySelector(".number-4")
+let number5_DOM = document.querySelector(".number-5")
+let number6_DOM = document.querySelector(".number-6")
+let number7_DOM = document.querySelector(".number-7")
+let number8_DOM = document.querySelector(".number-8")
+let number9_DOM = document.querySelector(".number-9")
+let plus_DOM = document.querySelector(".plus-btn")
+let minus_DOM = document.querySelector(".minus-btn")
+let multiply_DOM = document.querySelector(".times-btn")
+let divide_DOM = document.querySelector(".divide-btn")
+let dot_DOM = document.querySelector(".dot-btn")
+let equals_DOM = document.querySelector(".equals-btn")
+let delete_DOM = document.querySelector(".delete-btn")
+let reset_DOM = document.querySelector(".reset-btn")
+
 
 
 
